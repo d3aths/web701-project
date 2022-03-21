c39a0f4902821bb9e57769d8c8130f713da16d18
d3aths <feels.blindx@gmail.com>
2022-03-21 17:01:23 +1300

    started api commands

--- server/db/connection.js
+++ server/db/connection.js
@@ -13,7 +13,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("charity");
+        _db = db.db("web7");
         console.log("Successfully connected to MongoDB.")
       }
       return callback(err);
